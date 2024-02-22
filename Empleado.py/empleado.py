afa3015f5828c5cd12502304ad3b0e4fcc9da1ec
santiago2509 <santiagourbina2509@gmail.com>
2024-02-22 12:41:41 -0500

    nuevo

--- Empleado.py/empleado.py
+++ Empleado.py/empleado.py
@@ -4,4 +4,23 @@ class empleado:
     apellido= ""
     genero= ""
     # 1= masculino y 2= femenino
-    saldo= 0
+    saldo = 0
+    
+    '''# METODOS'''
+    def cambiarSalario(self, nuevoSalario):
+        # aqui va el codigo del metodo
+         return 0
+    def cambiarEmpleado(self, nuevoEmpleado):
+        # aqui va el codigo del nuevo empleado
+         return None
+     
+    def consultarSalario(self):
+        # aqui va el codigo del metodo
+         return self.saldo
+     
+    def consultarNombre(self):
+        return self.nombre
+    def consultarApellido(self):
+        return self.apellido}
+    
+        
